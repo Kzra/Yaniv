18f02598d81bfb1514490b1bdef89bac09392398
kzra <ezrakitson@gmail.com>
2018-12-30 10:05:19 +0000

    Edited script to delete redundant variables

--- Yaniv.py
+++ Yaniv.py
@@ -19,13 +19,6 @@
 import random
 
 #Variables#
-Full_Deck = [
-             'AH','2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH',
-             'AD','2D','3D','4D','5D','6D','7D','8D','9D','10D','JD','QD','KD',
-             'AS','2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS',
-             'AC','2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC',
-             'XX','XX'
-            ]
 Discard_Deck = [
                  ]
 Card_Values = {
@@ -693,15 +686,13 @@ while play == False:
        
         Yaniv = False
         
-        #reset the deck (strange behaviour, requires Full_Deck variable to be restated inside while loop)
-        Full_Deck = [
+        Current_Deck = [
              'AH','2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH',
              'AD','2D','3D','4D','5D','6D','7D','8D','9D','10D','JD','QD','KD',
              'AS','2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS',
              'AC','2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC',
              'XX','XX'
             ]
-        Current_Deck = Full_Deck
        
         
         #Deal out cards
